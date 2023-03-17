d26648e52435860909c8bb666b8556a24fffa735
RushikeshR1992 <128029046+RushikeshR1992@users.noreply.github.com>
2023-03-17 22:29:15 +0530

    Add files via upload

---
+++ Rolno,age,birthdate.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+	main ()
+	{
+	
+		int r,a,b;
+		char c;
+		
+			printf("Enter the Name:");
+			scanf("%c",&c);
+			
+			printf("Enter The Roll Number:");
+			scanf("%d",&r);
+			
+			printf("\n Enter The Age:");
+			scanf("%d",&a);
+			
+			printf("\n Enter The Birthday:");
+			scanf("%d",&b);
+			
+		return 0;
+	}		
