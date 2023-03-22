69d24d5dce250464b81d66d393772f643cfad29c
RushikeshR1992 <128029046+RushikeshR1992@users.noreply.github.com>
2023-03-22 09:56:28 -0700

    Add files via upload

---
+++ oddnumbers for loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+		int i,n,sum=0;
+		
+		printf("Enter the Any numbers");
+		scanf("%d",&n);
+
+		for(i=1;i<=n;i++)
+			
+			if(i%2==1)
+			{
+					printf("%d, ",i);
+			}
+}
