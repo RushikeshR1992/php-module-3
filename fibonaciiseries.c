604e52fc05657eac32b7b1c391bee8be3465502c
RushikeshR1992 <128029046+RushikeshR1992@users.noreply.github.com>
2023-03-22 10:26:43 -0700

    Add files via upload

---
+++ fibonaciiseries.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+			//0+1=1,  1+1=2,  1+2=3,  2+3=5
+	
+{
+		int n1=0,n2=1,n3,i,n;
+		
+		printf("Enter the any numbers:");
+		scanf("%d",&n);
+		
+		printf("%d %d ",n1,n2);
+		for(i<=2;i<=n;i++)
+		{
+			n3=n1+n2;
+			printf(" %d ",n3);
+			n1=n2;
+			n2=n3;
+		}
+		
+		
+}
